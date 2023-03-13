a9f526159b55ddccfa91d7421aaf94aa8046dd0a
RudyPavel <rudyy.pavlik@mail.ru>
2023-03-13 22:55:33 +0200

    zadanie

---
+++ zd41/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите M чисел...");
+string line = Console.ReadLine();
+
+string[] lineArr = line.Split(' ');
+int[] M = Array.ConvertAll(lineArr, int.Parse);
+
+int count = 0;
+
+for (int i = 0; i < M.Length; i++)
+{
+    if(M[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(count);
---
+++ zd43/Program.cs
@@ -0,0 +1,18 @@
+﻿string line1 = Console.ReadLine();
+string line2 = Console.ReadLine();
+
+string[] line1Arr = line1.Split(' ');
+double[] vector1 = Array.ConvertAll(line1Arr, double.Parse);
+string[] line2Arr = line2.Split(' ');
+double[] vector2 = Array.ConvertAll(line2Arr, double.Parse);
+
+double b1 = vector1[0];
+double k1 = vector1[1];
+
+double b2 = vector2[0];
+double k2 = vector2[1];
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"-> ({x},{y})");
